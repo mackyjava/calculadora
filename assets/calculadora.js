2044772592fbae92e5c6a8e1e8dd325dfdce6a92
maggie jv <maggie jv>
2017-03-23 17:02:00 -0600

    funciona la suma resultado en console

--- assets/calculadora.js
+++ assets/calculadora.js
@@ -13,13 +13,49 @@ function guardar(input){
         document.getElementById("numeros").value="";
     }console.log(nuevo);
 }
+var resultado =[];
 function sumar(){
-  var numeros=nuevo.join("+");
-  var res =0;
-  console.log(numeros);
-  for (var i=0;i<=numeros.length;i++){
-    res+=(numeros[i]);
+  var operacion=nuevo.join("+")
+  var num =(Array.from(nuevo));
+  var res=0;
+  console.log(num);
+  for (var i=0;i<=num.length-1;i++){
+    res+=(Number(num[i]));
+}
+  //resultado.push(res);
+   console.log(res);
+}
+function restar(){
+  var operacion=nuevo.join("+")
+  var num =(Array.from(nuevo));
+  var res=0;
+  console.log(num);
+  for (var i=0;i<=num.length-1;i++){
+    console.log(res=(Number(num[i]))-(Number(num[i+1])));
 
   }
-  console.log(parseInt(res));
+  console.log(res);
+}
+
+function mult(){
+  var operacion=nuevo.join("+")
+  var num =(Array.from(nuevo));
+  var res=0;
+  console.log(num);
+  for (var i=0;i<=num.length-1;i++){
+    res*=(Number(num[i]));
+
+  }
+  console.log(res);
+}
+function div(){
+  var operacion=nuevo.join("+")
+  var num =(Array.from(nuevo));
+  var res=0;
+  console.log(num);
+  for (var i=0;i<=num.length-1;i++){
+    res/=(Number(num[i]));
+
+  }
+  console.log(res.toFixed(2));
 }
