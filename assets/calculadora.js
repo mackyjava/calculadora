faf440b6a713de3d511c03bc2e018ad7ec625c11
maggie jv <maggie jv>
2017-03-24 15:55:33 -0600

    condicionales para las funciones

--- assets/calculadora.js
+++ assets/calculadora.js
@@ -7,6 +7,7 @@
 }
 var nuevo=[];
 var check=Boolean;
+var oper="";
 function guardar(input){
     var entry= document.getElementById("numeros");
      if(entry.value!=0){
@@ -15,54 +16,59 @@ function guardar(input){
     }console.log(nuevo);
 }
 var operacionAnterior =[];
-    function sumar(){
-      var ins=document.getElementById("numeros")
-      var numeroS= nuevo.toString().split("+")
-      var res=0;
-      for (var i=0;i<=numeroS.length-1;i++){
-      res+=Number(numeroS[i]);
-  }
-   operacionAnterior.push(res);
-   ins.innerHTML= res;
-   check= true;
+function operar(operar) {
+  document.getElementById("resultado").value = operar;
+  oper = operar;
 }
+function operaciones(){
+  if(oper=="+"){
+        function sumar(){
+          var numeroS= nuevo.toString().split("+");
+          var res=0;
+          for (var i=0;i<=numeroS.length-1;i++){
+        res+=Number(numeroS[i]);
+    }return  res;
+     operacionAnterior.push(res);
+      document.getElementById("numeros").innerHTML= res;
+     console.log(res)
+   }
+  }
+ if(oper=="-"){
+       function restar(){
+          var insertar=document.getElementById("numeros");
+          var numeroS= nuevo.toString().split("-");
+          var res=0;
+          for (var i=0;i<=numeroS.length-2;i++){
+          res=Number(numeroS[i])- Number(numeroS[i+1]);
+        }
+          operacionAnterior.push(res);
 
-function restar(){
-  var ins=document.getElementById("numeros")
-  var numeroS= nuevo.toString().split("-")
-  var res=0;
-  for (var i=0;i<=numeroS.length-2;i++){
-    res=Number(numeroS[i])- Number(numeroS[i+1]);
-}
-   operacionAnterior.push(res);
-   ins.innerHTML= res;
-   check= true;
- }
- function mult(){
-  var ins=document.getElementById("numeros")
-  var numeroS= nuevo.toString().split("*")
-  var res=1;
-  for (var i=0;i<=numeroS.length-1;i++){
-    res*=Number(numeroS[i])
-}
-   operacionAnterior.push(res);
-   ins.innerHTML= res;
- }
- function divi(){
-   var ins=document.getElementById("numeros")
-   var numeroS= nuevo.toString().split("/")
-   var res=0;
-   for (var i=0;i<=numeroS.length-2;i++){
-     res=Number(numeroS[i])/Number(numeroS[i+1])
- }
-    operacionAnterior.push(res);
-    ins.innerHTML= res.toFixed(2);
-  }
-function resultado(){
-  var aDb=nuevo.toString().split("");   //arrayDebusqueda
-  var ind = aDb.indexOf("+");
-  var operadores= document.getElementsByName('operador');
-  if(check=="true"&& operadores.value=="+"){
-      sumar();
+          console.log(res)
+        }return ins.innerHTML= res;
+      }
+  		if(oper=="*"){
+         function mult(){
+           var ins=document.getElementById("numeros")
+           var numeroS= nuevo.toString().split("*")
+           var res=1;
+           for (var i=0;i<=numeroS.length-1;i++){
+               res*=Number(numeroS[i])
+      }
+           operacionAnterior.push(res);
+
+      }return  ins.innerHTML= res;
+    }
+       if(oper=="/"){
+        function divi(){
+          var ins=document.getElementById("numeros")
+          var numeroS= nuevo.toString().split("/")
+          var res=0;
+          for (var i=0;i<=numeroS.length-2;i++){
+            res=Number(numeroS[i])/Number(numeroS[i+1])
+      }
+         operacionAnterior.push(res);
+
+       }return ins.innerHTML= res.toFixed(2);
+
 }
 }
